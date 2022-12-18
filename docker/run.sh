c667176db87beebf71f638f39acdbba8a7b88913
Samuel Maderson <samuel.maderson@oliveiratrust.com.br>
2022-12-18 17:44:32 -0300

    added dockerfile & gitignore

---
+++ docker/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export AWS_ACCESS_KEY_ID=AKIAQHZPZUUE2ER47X3C
+export AWS_SECRET_ACCESS_KEY=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
+export AWS_DEFAULT_REGION=us-east-1
+
+cd /opt/eksRancherDeploy && ts-node ts-node main.ts
